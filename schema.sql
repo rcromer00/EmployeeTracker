69db57259907a8b3d8f4ba9614785f8226434347
rcromer00 <cromerro@msu.edu>
2021-01-11 10:05:09 -0500

    creates database

---
+++ schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+ CREATE TABLE department(
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+  CREATE TABLE role(
+  id INT AUTO_INCREMENT NOT NULL,
+   title VARCHAR(30),
+   salary DECIMAL(10,3),
+   department_id INT NOT NULL,
+   PRIMARY KEY (id),
+   
+   FOREIGN KEY (department_id)
+   REFERENCES department(id)
+  );
+
+CREATE TABLE employee(
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES role(id)
+);
